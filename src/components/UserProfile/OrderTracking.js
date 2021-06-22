34ab21693f7a69f6b1434ae70503b5ff074533a2
Mrhb787 <lit2019008@iiitl.ac.in>
2021-06-22 16:50:26 +0530

    Order Tracking Fixes and Improvements, Cart Fixes

--- src/components/UserProfile/OrderTracking.js
+++ src/components/UserProfile/OrderTracking.js
@@ -22,10 +22,36 @@ function OrderTracking() {
             Expected Arrival <span style={ArrivalDate}>22/06/2021</span>
           </div>
         </div>
+        <div className="order-details">
+          <img
+            src="./images/book-of-black-cover-closed.png"
+            alt=""
+            width="150"
+          />
+          <p>
+            <ul style={{listStyle: "none"}}>
+              <li>
+                <i class="fas fa-circle-notch"></i>&nbsp;Book Name
+              </li>
+              <li>
+                <i class="fas fa-circle-notch"></i>&nbsp;Book ISBN
+              </li>
+              <li>
+                <i class="fas fa-circle-notch"></i>&nbsp;Book Author
+              </li>
+              <li>
+                <i class="fas fa-circle-notch"></i>&nbsp;Seller Name
+              </li>
+            </ul>
+          </p>
+        </div>
         <div className="order-status">
           <span className="checkpoint">
             {/* change className to = "far fa-circle for empty circle" */}
             <i className="fas fa-check-circle" />
+            {/* add below two lines when checkpoint is reached for sonar effect */}
+            <i className="far fa-circle sonar-wave1 wave" />
+            <i className="far fa-circle sonar-wave2 wave" />
             {/* change className to = "fas fa-check-circle" for filled circle */}
           </span>
           <span className="progress-bar">
@@ -35,22 +61,40 @@ function OrderTracking() {
                 className = "filled-75" for 75% filled bar
                 className = "filled-100" for 100% filled bar
             */}
-            <div className="filled-75"></div>
+            <div className="filled-75 animated-filled"></div>
           </span>
           <span className="checkpoint">
+            {/* change className to = "far fa-circle for empty circle" */}
             <i className="far fa-circle" />
+            {/* add below two lines when checkpoint is reached for sonar effect */}
+            {/* 
+                <i className="far fa-circle sonar-wave1 wave" />
+                <i className="far fa-circle sonar-wave2 wave" /> 
+            */}
           </span>
           <span className="progress-bar">
-            <div className="filled-0"></div>
+            <div className="filled-0 animated-filled"></div>
           </span>
           <span className="checkpoint">
+            {/* change className to = "far fa-circle for empty circle" */}
             <i className="far fa-circle" />
+            {/* add below two lines when checkpoint is reached for sonar effect */}
+            {/* 
+                <i className="far fa-circle sonar-wave1 wave" />
+                <i className="far fa-circle sonar-wave2 wave" /> 
+            */}
           </span>
           <span className="progress-bar">
-            <div className="filled-0"></div>
+            <div className="filled-0 animated-filled"></div>
           </span>
           <span className="checkpoint">
+            {/* change className to = "far fa-circle for empty circle" */}
             <i className="far fa-circle" />
+            {/* add below two lines when checkpoint is reached for sonar effect */}
+            {/* 
+                <i className="far fa-circle sonar-wave1 wave" />
+                <i className="far fa-circle sonar-wave2 wave" /> 
+            */}
           </span>
         </div>
         <div className="order-status-icons">
@@ -86,6 +130,16 @@ function OrderTracking() {
               Delivered
             </b>
           </p>
+        </div>
+        <div className="order-tracking-buttons">
+          <div className="cancel-order-button">
+            <i class="fas fa-window-close" />
+            &nbsp;&nbsp;Cancel Order
+          </div>
+          <div className="download-receipt-button">
+            <i class="fas fa-download" />
+            &nbsp;&nbsp;Receipt
+          </div>
         </div>
       </div>
     </div>
