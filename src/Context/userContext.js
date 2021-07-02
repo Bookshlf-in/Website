cf1c4b589b02260efa2f8740d8388b7b9d278993
Rohit Kumar <lit2019037@iiitl.ac.in>
2021-07-03 02:31:52 +0530

    added localstorage to userContext and login

--- src/Context/userContext.js
+++ src/Context/userContext.js
@@ -3,7 +3,9 @@ import React, { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export const CurrentUserProvider = (props) => {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("bookshlf_user"))
+  );
 
   return (
     <UserContext.Provider value={[user, setUser]}>
