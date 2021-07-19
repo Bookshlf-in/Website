4eaf2c09adc9b820d57a33b1485f1dcc64b8f7dd
Mrhb787 <lit2019008@iiitl.ac.in>
2021-07-19 16:48:06 +0530

    Past Orders API Completed
    other :  Featured Categories Enhanced, Carosel Enhanced,

--- src/components/UserProfile/PreviousOrders.js
+++ src/components/UserProfile/PreviousOrders.js
@@ -1,58 +1,85 @@
-import React from "react";
+import {React, useState} from "react";
 import {Link} from "react-router-dom";
 
-function PreviousOrders() {
+const PreviousOrders = (props) => {
+  const [pastOrders, setpastOrders] = useState(props.orders);
   return (
     <div className="user-previous-orders" id="user-previous-orders">
       <table>
-        <tr>
-          <th> Order ID </th>
-          <th> Dated </th>
-          <th> Details </th>
-          <th> Price </th>
-          <th> Payment Method</th>
-          <th> Reviews</th>
-        </tr>
-        {/* item order details */}
-        <tr>
-          <td> #BJSAOSAJS </td>
-          <td>Dated : 15 June, 2021</td>
-          <td>
-            <ul style={{listStyle: "none"}}>
-              <li>Address : Bhopal Madhyapradesh 200781</li>
-              <li>Book : HC Verma Physcis Vol1 </li>
-              <li>ISBN:##########</li>
-            </ul>
-          </td>
-          <td>
-            <i className="fas fa-rupee-sign"></i> 540/-
-          </td>
-          <td>Cash</td>
-          <td>
-            <Link className="tracking-order-link" to="/AddReview">Review this Order</Link>
-          </td>
-        </tr>
-        {/* item order details */}
-        <tr>
-          <td> #BJSAOSAJS </td>
-          <td>Dated : 15 June, 2021</td>
-          <td>
-            <ul style={{listStyle: "none"}}>
-              <li>Address : Bhopal Madhyapradesh 200781</li>
-              <li>Book : HC Verma Physcis Vol1 </li>
-              <li>ISBN:##########</li>
-            </ul>
-          </td>
-          <td>
-            <i className="fas fa-rupee-sign"></i> 540/-
-          </td>
-          <td>Cash</td>
-          <td>
-            <Link className="tracking-order-link" to="/AddReview">Review this Order</Link>
-          </td>
-        </tr>
+        <thead>
+          <tr>
+            <th> Order ID </th>
+            <th> Seller </th>
+            <th> Book Details </th>
+            <th> Order Total </th>
+            <th> Status</th>
+            <th> Reviews</th>
+          </tr>
+        </thead>
+        {pastOrders && pastOrders.length > 0 ? (
+          <tbody>
+            {pastOrders.map((order, idx) => (
+              <tr key={idx}>
+                <td> {order._id} </td>
+                <td>{order.sellerName}</td>
+                <td>
+                  <ul style={{listStyle: "none"}}>
+                    <li>
+                      <b>Book</b> : {order.title}
+                    </li>
+                    <li>
+                      <b>QTY</b> : {order.purchaseQty}
+                    </li>
+                    <li>
+                      <b>Author</b> : {order.author}
+                    </li>
+                    <li>
+                      <b>Price</b> : <i className="fas fa-rupee-sign" />
+                      {" " + order.price + " /-"}
+                    </li>
+                  </ul>
+                </td>
+                <td>
+                  <i className="fas fa-rupee-sign" />
+                  {" " + order.orderTotal + " /-"}
+                </td>
+                <td
+                  style={{
+                    backgroundColor:
+                      order.status[order.status.length - 1] === "Delivered"
+                        ? "yellowgreen"
+                        : "red",
+                    color: "white",
+                  }}
+                >
+                  {order.status[order.status.length - 1]}
+                </td>
+                <td>
+                  {order.status[order.status.length - 1] === "Delivered" ? (
+                    <Link className="tracking-order-link" to="/AddReview">
+                      Review this Order
+                    </Link>
+                  ) : (
+                    "---"
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        ) : (
+          <tfoot>
+            <tr>
+              <td>No Orders...</td>
+              <td>---</td>
+              <td>---</td>
+              <td>---</td>
+              <td>---</td>
+              <td>---</td>
+            </tr>
+          </tfoot>
+        )}
       </table>
     </div>
   );
-}
+};
 export default PreviousOrders;
