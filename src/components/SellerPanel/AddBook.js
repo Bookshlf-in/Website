091d5b889c5bb30966f8435737b03f78b0e19c58
Mrhb787 <lit2019008@iiitl.ac.in>
2021-10-02 15:55:27 +0530

    Add Book Component Improved

--- src/components/SellerPanel/AddBook.js
+++ src/components/SellerPanel/AddBook.js
@@ -1,9 +1,8 @@
 import {React, useState} from "react";
-import {Link} from "react-router-dom";
+import {Link, useParams, useHistory} from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import {makeStyles} from "@material-ui/core/styles";
 import Checkbox from "@material-ui/core/Checkbox";
-import Icon from "@material-ui/core/Icon";
 import axios from "../../axios";
 import Alert from "@material-ui/lab/Alert";
 import {storage} from "../../firebase";
@@ -23,12 +22,12 @@ const useStyles = makeStyles((theme) => ({
 
 const AddBook = (props) => {
   const classes = useStyles();
-
+  const history = useHistory();
   // book Details states
   const [bookName, setbookName] = useState("");
-  const [bookISBN, setbookISBN] = useState("");
+  const [bookISBN, setbookISBN] = useState("9782724088526");
   const [SP, setSP] = useState("");
-  const [MRP, setMRP] = useState("");
+  const [Earn, setEarn] = useState("");
   const [bookDesc, setbookDesc] = useState("");
   const [Weight, setWeight] = useState("");
   const [Edition, setEdition] = useState("");
@@ -62,9 +61,9 @@ const AddBook = (props) => {
 
   const Initialize = () => {
     setbookName("");
-    setbookISBN("");
+    setbookISBN("9782724088526");
     setSP("");
-    setMRP("");
+    setEarn("");
     setbookDesc("");
     setWeight("");
     setEdition("");
@@ -140,7 +139,7 @@ const AddBook = (props) => {
           axios
             .post("/addBook", {
               title: bookName,
-              MRP: Number(MRP),
+              MRP: Number(Earn),
               price: Number(SP),
               editionYear: Number(Edition),
               author: author,
@@ -197,7 +196,6 @@ const AddBook = (props) => {
   function precheck(imglinks) {
     return (
       bookName !== "" &&
-      MRP !== "" &&
       SP !== "" &&
       Edition !== "" &&
       author !== "" &&
@@ -207,7 +205,8 @@ const AddBook = (props) => {
       bookDesc !== "" &&
       imglinks !== undefined &&
       imglinks !== null &&
-      imglinks.length >= 3
+      imglinks.length >= 3 &&
+      Qnty > "0"
     );
   }
   const handelBookAdd = async () => {
@@ -215,6 +214,26 @@ const AddBook = (props) => {
     await PushDetails(imglinks);
   };
 
+  const FindEarnings = (sp) => {
+    let price = Number(sp);
+    let earnings = 0;
+
+    if (price > 50 && price <= 100) earnings = 50 + (price * 3) / 100;
+    else if (price > 100 && price <= 500) earnings = 100 + (price * 3) / 100;
+    else if (price > 500 && price <= 1000) earnings = 200 + (price * 3) / 100;
+    else if (price > 1000 && price <= 1500) earnings = 300 + (price * 3) / 100;
+    else if (price > 1500 && price <= 2000) earnings = 400 + (price * 3) / 100;
+    else if (price > 2000 && price <= 2500) earnings = 500 + (price * 3) / 100;
+    else if (price > 2500 && price <= 3000) earnings = 600 + (price * 3) / 100;
+    else if (price > 3000 && price <= 3500) earnings = 700 + (price * 3) / 100;
+    else if (price > 3500 && price <= 4000) earnings = 800 + (price * 3) / 100;
+    else if (price > 4000 && price <= 4500) earnings = 900 + (price * 3) / 100;
+    else if (price > 4500 && price <= 5000) earnings = 1000 + (price * 3) / 100;
+    else if (price > 5000) earnings = 1500 + (price * 3) / 100;
+    else earnings = price;
+    setEarn(Math.round(price - earnings));
+    // console.log("Your Earnings : ", earnings);
+  };
   return (
     <div className="add-book-bg">
       <h1> ADD NEW BOOK </h1>
@@ -232,34 +251,6 @@ const AddBook = (props) => {
         </div>
         <div className="add-book-field1">
           <span>
-            <i className="fas fa-atlas"></i>
-          </span>
-          <input
-            type="text"
-            placeholder="Book ISBN Number"
-            onChange={(e) => setbookISBN(e.target.value)}
-            value={bookISBN}
-          />
-        </div>
-        <div className="add-book-field2">
-          <span>
-            <i className="fas fa-rupee-sign"></i>
-          </span>
-          <input
-            type="number"
-            placeholder="Selling Price"
-            onChange={(e) => setSP(e.target.value)}
-            value={SP}
-          />
-          <input
-            type="number"
-            placeholder="M.R.P"
-            onChange={(e) => setMRP(e.target.value)}
-            value={MRP}
-          />
-        </div>
-        <div className="add-book-field1">
-          <span>
             <i className="fas fa-info"></i>
           </span>
           <input
@@ -269,18 +260,86 @@ const AddBook = (props) => {
             value={bookDesc}
           />
         </div>
+        {/* <div className="add-book-field1">
+        <span>
+          <i className="fas fa-atlas"></i>
+        </span>
+        <input
+          type="text"
+          placeholder="Book ISBN Number"
+          onChange={(e) => setbookISBN(e.target.value)}
+          value={bookISBN}
+        />
+        </div> */}
+        <div className="add-book-field2">
+          <span>
+            <i className="fas fa-rupee-sign"></i>
+          </span>
+          <input
+            type="number"
+            placeholder="Selling Price"
+            onChange={(e) => {
+              setSP(e.target.value);
+              FindEarnings(e.target.value);
+            }}
+            value={SP}
+          />
+          <input
+            type="Text"
+            placeholder="Your Earnings (₹)"
+            value={Earn}
+            readOnly
+          />
+        </div>
+        <div className="add-book-field1">
+          <span>
+            <i className="fas fa-address-book" />
+          </span>
+          <select
+            name=""
+            id=""
+            style={{
+              height: "50px",
+              outline: "none",
+              fontFamily: "PT Sans",
+              paddingLeft: "10px",
+              backgroundColor: "rgba(255, 255, 255, 0.87)",
+              border: "none",
+            }}
+            onChange={(e) => {
+              setPickupId(e.target.value);
+              if (e.target.value === "addNew") {
+                history.push("/SellerPanel/3");
+              }
+            }}
+          >
+            <option> Select Address</option>
+            {Adr !== null ? (
+              <>
+                {Adr.map((adr, idx) => (
+                  <option value={adr._id} key={idx}>
+                    {adr.address}
+                  </option>
+                ))}
+              </>
+            ) : (
+              <></>
+            )}
+            <option value="addNew">Add New Address</option>
+          </select>
+        </div>
         <div className="add-book-field1">
           <span>
             <i className="fas fa-weight"></i>
           </span>
           <input
             type="number"
-            placeholder="Weight in grams(if possible)"
+            placeholder="Weight in grams (if possible)"
             onChange={(e) => setWeight(e.target.value)}
             value={Weight}
           />
         </div>
-        <div className="add-book-field1">
+        {/* <div className="add-book-field1">
           <span>
             <i className="fab fa-etsy"></i>
           </span>
@@ -290,7 +349,7 @@ const AddBook = (props) => {
             onChange={(e) => setEdition(e.target.value)}
             value={Edition}
           />
-        </div>
+        </div> */}
         <div className="add-book-field1">
           <span>
             <i className="fas fa-user-edit" />
@@ -324,39 +383,7 @@ const AddBook = (props) => {
             value={lang}
           />
         </div>
-        <div className="add-book-field1">
-          <span>
-            <i className="fas fa-address-book" />
-          </span>
-          <select
-            name=""
-            id=""
-            style={{
-              height: "50px",
-              outline: "none",
-              fontFamily: "PT Sans",
-              paddingLeft: "10px",
-              backgroundColor: "rgba(255, 255, 255, 0.87)",
-              border: "none",
-            }}
-            onChange={(e) => {
-              setPickupId(e.target.value);
-            }}
-          >
-            <option> Select Address</option>
-            {Adr !== null ? (
-              <>
-                {Adr.map((adr, idx) => (
-                  <option value={adr._id} key={idx}>
-                    {adr.address}
-                  </option>
-                ))}
-              </>
-            ) : (
-              <></>
-            )}
-          </select>
-        </div>
+
         <div className="add-book-field1">
           <span>
             <i className="fab fa-youtube" />
