6680ce82494cef6c90e980fe54cbf5a3bdfc3a54
yash jain <yashsj24@gmail.comgit config --global user.name yash>
2021-11-13 00:47:08 +0530

    add magnifier for book image

--- src/components/BookDetails/Bookfullsnap.js
+++ src/components/BookDetails/Bookfullsnap.js
@@ -1,9 +1,66 @@
 import React from "react";
 function Bookfullsnap(props) {
+  function handleLoad() {
+    var img = document.getElementById("book-snaps-full");
+    var glass = document.getElementById("glass");
+    glass.style.backgroundImage = "url('" + img.src + "')";
+  }
+  function handleMove(e) {
+    e.preventDefault();
+    var img = document.getElementById("book-snaps-full");
+    var glass = document.getElementById("glass");
+    var w, h, pos, x, y;
+    // glass.style.backgroundImage = "url('" + props.url + "')";
+    glass.style.backgroundRepeat = "no-repeat";
+    glass.style.backgroundSize = img.width * 2 + "px " + img.height * 2 + "px";
+    w = glass.offsetWidth / 2;
+    h = glass.offsetHeight / 2;
+    pos = getCurrPos(e, img);
+    x = pos.x;
+    y = pos.y;
+    if (x > img.width - w / 2) {
+      x = img.width - w / 2;
+    }
+    if (x < w / 2) {
+      x = w / 2;
+    }
+    if (y > img.height - h / 2) {
+      y = img.height - h / 2;
+    }
+    if (y < h / 2) {
+      y = h / 2;
+    }
+    glass.style.left = x - w + "px";
+    glass.style.top = y - h + "px";
+    glass.style.backgroundPosition =
+      "-" + (x * 2 - w) + "px -" + (y * 2 - h) + "px";
+  }
+  function getCurrPos(e, img) {
+    var a,
+      x = 0,
+      y = 0;
+    e = e || window.event;
+    a = img.getBoundingClientRect();
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return { x: x, y: y };
+  }
+
   return (
     <div>
       <div className="book-snaps-full" id="book-snaps">
+        <div
+          className="img-magnifier-glass"
+          onMouseMove={handleMove}
+          onTouchMove={handleMove}
+          id="glass"
+        ></div>
         <img
+          onLoad={handleLoad}
+          onTouchMove={handleMove}
+          onMouseMove={handleMove}
           src={props.url}
           alt=""
           height="100%"
