b4467618c943708a999dadf4d89d52a6dbca69df
Abhishek Singh <lit2019008@iiitl.ac.in>
2022-01-23 00:34:54 +0530

    removed https-redirect

--- src/index.js
+++ src/index.js
@@ -5,18 +5,14 @@ import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import { CurrentUserProvider } from "./Context/userContext";
 import { CurrentFormProvider } from "./Context/formContext";
-import HttpsRedirect from "react-https-redirect";
 ReactDOM.render(
   <React.StrictMode>
     <CurrentUserProvider>
       <CurrentFormProvider>
-        <HttpsRedirect>
-          <App />
-        </HttpsRedirect>
+        <App />
       </CurrentFormProvider>
     </CurrentUserProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
 reportWebVitals();
