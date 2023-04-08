7430804230c3ab80462020ec50c5226595fd7a9c
Mrhb787 <lit2019008@iiitl.ac.in>
2023-04-09 01:16:06 +0530

    checkout small changes

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -12,8 +12,6 @@ import axios from "../../axios";
 import { Grid, Stack, Divider } from "@mui/material";
 import { Alert, AlertTitle, Skeleton, Typography, Chip } from "@mui/material";
 import { Button, TextField } from "@mui/material";
-import { InputLabel, MenuItem, FormControl, Select } from "@mui/material";
-import Popover from "@mui/material/Popover";
 
 import LoadingButton from "@mui/lab/LoadingButton";
 
@@ -30,6 +28,15 @@ import CheckIcon from "@mui/icons-material/CheckCircle";
 import CheckoutLoading from "./CheckoutLoading";
 import CheckoutStepper from "./CheckoutStepper";
 import CheckoutAddress from "./CheckoutAddress";
+import CheckoutPayment from "./CheckoutPayment";
+import CheckoutOrderReview from "./CheckoutOrderReview";
+
+const PanelTitle = [
+  "Shipping Address",
+  "Payment Details",
+  "Review Order",
+  "Place Order",
+];
 
 const Payment = () => {
   // Calling Hooks
@@ -51,7 +58,6 @@ const Payment = () => {
   const [checkout, setCheckout] = useState({});
   const [address, setAddress] = useState([]);
   const [addressId, setAddressId] = useState("");
-  const [addressIndex, setaddressIntex] = useState(0);
 
   useEffect(() => {
     const fetch = () => {
@@ -172,6 +178,7 @@ const Payment = () => {
                 maxWidth: 600,
                 minWidth: 350,
               }}
+              spacing={2}
             >
               {panel === 0 ? (
                 <CheckoutAddress
@@ -181,285 +188,47 @@ const Payment = () => {
                   setAddressId={setAddressId}
                 />
               ) : panel === 1 ? (
-                <Stack
-                  direction="column"
-                  justifyContent="center"
-                  alignItems="center"
-                  sx={{
-                    padding: "20px 10px",
-                    border: "1px solid rgba(0,0,0,0.1)",
-                    borderRadius: "5px",
-                  }}
-                  spacing={5}
-                >
-                  <Stack
-                    direction="column"
-                    spacing={1}
-                    divider={<Divider orientation="horizontal" flexItem />}
-                    sx={{ width: "100%", maxWidth: 800 }}
-                  >
-                    <Stack direction="row" justifyContent="space-between">
-                      <Typography
-                        variant="h6"
-                        sx={{ fontFamily: "Staatliches", letterSpacing: "1px" }}
-                      >
-                        Item ({checkout.totalItems})
-                      </Typography>
-                      <Typography
-                        align="right"
-                        variant="h6"
-                        sx={{
-                          fontFamily: "Staatliches",
-                          letterSpacing: "1px",
-                        }}
-                      >
-                        Price (<RupeeIcon sx={{ height: 15, width: 15 }} />)
-                      </Typography>
-                    </Stack>
-                    {checkoutType !== "cart" ? (
-                      <Stack direction="row" justifyContent="space-between">
-                        <Typography variant="caption">
-                          {checkout.item.title}
-                        </Typography>
-                        <Typography align="right" variant="caption">
-                          {checkout.item.price} x {checkout.item.purchaseQty}
-                        </Typography>
-                      </Stack>
-                    ) : (
-                      checkout.items.map((item) => (
-                        <Stack
-                          direction="row"
-                          justifyContent="space-between"
-                          key={item._id}
-                        >
-                          <Typography variant="caption">
-                            {item.title}
-                          </Typography>
-                          <Typography align="right" variant="caption">
-                            {item.price} x {item.purchaseQty}
-                          </Typography>
-                        </Stack>
-                      ))
-                    )}
-                    <Stack
-                      direction="row"
-                      justifyContent="flex-end"
-                      spacing={5}
-                    >
-                      <Typography variant="caption">Items Subtotal</Typography>
-                      <Chip
-                        icon={<RupeeIcon />}
-                        label={checkout.itemsSubtotal}
-                        size="small"
-                        color="secondary"
-                      />
-                    </Stack>
-                    <Stack
-                      direction="row"
-                      justifyContent="flex-end"
-                      spacing={5}
-                    >
-                      <Typography variant="caption">
-                        Shipping Charges
-                      </Typography>
-                      <Chip
-                        icon={<RupeeIcon />}
-                        label={checkout.shippingCharges}
-                        size="small"
-                        color="info"
-                      />
-                    </Stack>
-                    <Stack
-                      direction="row"
-                      justifyContent="flex-end"
-                      spacing={5}
-                    >
-                      <Typography variant="caption">Order Total</Typography>
-                      <Chip
-                        icon={<RupeeIcon />}
-                        label={checkout.orderTotal}
-                        size="small"
-                        color="success"
-                      />
-                    </Stack>
-                  </Stack>
-                  <Stack
-                    direction="row"
-                    justifyContent="space-between"
-                    sx={{ width: "100%", maxWidth: 800 }}
-                    spacing={2}
-                  >
-                    <Button
-                      startIcon={<BackIcon />}
-                      variant="contained"
-                      sx={{ letterSpacing: "1px", fontSize: "12px" }}
-                      onClick={PrevPanel}
-                      size="small"
-                      color="warning"
-                    >
-                      Go to Address
-                    </Button>
-                    <Button
-                      endIcon={<NextIcon />}
-                      variant="contained"
-                      sx={{ letterSpacing: "1px", fontSize: "12px" }}
-                      onClick={NextPanel}
-                    >
-                      Review Your Order
-                    </Button>
-                  </Stack>
-                </Stack>
+                <CheckoutPayment
+                  checkout={checkout}
+                  checkoutType={checkoutType}
+                />
               ) : (
-                <Stack
-                  direction="column"
-                  sx={{ paddingTop: "20px" }}
-                  spacing={2}
-                >
-                  <Typography
-                    variant="h5"
-                    sx={{
-                      fontFamily: "Staatliches",
-                      letterSpacing: "1px",
-                    }}
-                  >
-                    Shipping Address
-                  </Typography>
-                  <Stack
-                    sx={{
-                      border: "1px solid rgba(0,0,0,0.1)",
-                      padding: "10px",
-                      borderRadius: "5px",
-                    }}
-                  >
-                    <Typography variant="body2">
-                      {address[addressIndex].label + " : "}
-                      <Typography variant="caption">
-                        {address[addressIndex].address +
-                          ", " +
-                          address[addressIndex].state +
-                          ", " +
-                          address[addressIndex].city +
-                          ", " +
-                          address[addressIndex].zipCode}
-                      </Typography>
-                    </Typography>
-                  </Stack>
-                  <Stack
-                    sx={{
-                      border: "1px solid rgba(0,0,0,0.1)",
-                      padding: "10px",
-                      borderRadius: "5px",
-                    }}
-                    direction={{
-                      xs: "column",
-                      sm: "row",
-                      lg: "row",
-                      md: "row",
-                    }}
-                    spacing={2}
-                  >
-                    <Typography variant="body2">Contact Number :</Typography>
-                    <Chip
-                      icon={<CallIcon />}
-                      label={address[addressIndex].phoneNo}
-                      size="small"
-                      color="primary"
-                    />
-                    {address[addressIndex].AltPhoneNo ? (
-                      <Chip
-                        icon={<CallIcon />}
-                        label={address[addressIndex].AltPhoneNo}
-                        size="small"
-                        color="warning"
-                      />
-                    ) : null}
-                  </Stack>
-                  <Typography
-                    variant="h5"
-                    sx={{
-                      fontFamily: "Staatliches",
-                      letterSpacing: "1px",
-                    }}
-                  >
-                    Payment Details
-                  </Typography>
-                  <Stack
-                    sx={{
-                      border: "1px solid rgba(0,0,0,0.1)",
-                      padding: "10px",
-                      borderRadius: "5px",
-                    }}
-                    direction={{
-                      xs: "column",
-                      sm: "row",
-                      lg: "row",
-                      md: "row",
-                    }}
-                    spacing={2}
-                  >
-                    <Typography variant="body2">Total Items :</Typography>
-                    <Chip label={checkout.totalItems} size="small" />
-                    <Typography variant="body2">Order Total :</Typography>
-                    <Chip
-                      icon={<RupeeIcon />}
-                      label={checkout.orderTotal}
-                      size="small"
-                    />
-                  </Stack>
-                  {stopCheckout ? (
-                    <Alert severity="warning">
-                      <AlertTitle>Out of Stock!</AlertTitle>
-                      <Typography variant="caption">
-                        Some Items have Either gone Out of Stock or Currently
-                        not Available. It May be Possible that Items Quantity
-                        have Gone less than You Require. We Suggest to Re-Place
-                        Your Order from Start.
-                      </Typography>
-                    </Alert>
-                  ) : null}
-                  <Stack
-                    direction="row"
-                    spacing={2}
-                    justifyContent="space-between"
-                    alignItems="center"
-                  >
-                    <Button
-                      startIcon={<BackIcon />}
-                      variant="contained"
-                      sx={{ letterSpacing: "1px", fontSize: "12px" }}
-                      onClick={PrevPanel}
-                      size="small"
-                      color="warning"
-                    >
-                      Go to Payments
-                    </Button>
-                    <LoadingButton
-                      loading={orderLoad}
-                      endIcon={orderPlaced ? <CheckIcon /> : <ShippingIcon />}
-                      variant="contained"
-                      sx={{ letterSpacing: "1px", fontSize: "12px" }}
-                      size="small"
-                      color="success"
-                      onClick={handelPlaceOrder}
-                      disabled={stopCheckout}
-                    >
-                      {orderPlaced
-                        ? "Order Placed Successfully"
-                        : orderLoad
-                        ? "Placing..."
-                        : "Place Order"}
-                    </LoadingButton>
-                  </Stack>
-                </Stack>
+                <CheckoutOrderReview
+                  address={address}
+                  addressId={addressId}
+                  checkout={checkout}
+                />
               )}
+              <Stack
+                direction="row"
+                sx={{ width: "100%" }}
+                justifyContent="space-between"
+                spacing={2}
+                alignItems="center"
+              >
+                {panel > 0 && (
+                  <Button
+                    onClick={PrevPanel}
+                    variant="outlined"
+                    startIcon={<BackIcon />}
+                    size="small"
+                    color="warning"
+                  >
+                    {PanelTitle[panel - 1]}
+                  </Button>
+                )}
+                {panel < 3 && (
+                  <Button
+                    onClick={NextPanel}
+                    variant="contained"
+                    endIcon={<NextIcon />}
+                    size="small"
+                  >
+                    {PanelTitle[panel + 1]}
+                  </Button>
+                )}
+              </Stack>
             </Stack>
-
-            <Button onClick={() => setPanel((prev) => prev + 1)}>
-              Next Panel
-            </Button>
-            <Button onClick={() => setPanel((prev) => prev - 1)}>
-              Prev Panel
-            </Button>
           </Stack>
         )
       ) : (
